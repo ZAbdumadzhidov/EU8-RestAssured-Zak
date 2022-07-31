a509a5558cddd45e001c07b14c1993a1078d16c2
Z.Abdumadzhidov <odil911@mail.ru>
2022-07-31 20:44:29 +0100

    SimpleGetRequest

--- src/test/java/com/cydeo/Day2/SimpleGetRequest.java
+++ src/test/java/com/cydeo/Day2/SimpleGetRequest.java
@@ -8,20 +8,11 @@ public class SimpleGetRequest {
 
     String url = "http://54.196.212.186:8000/api/spartans";
 
-    @Test
-    public void test1(){
 
-        //send a get request and save response inside the Request object
-        Response response = RestAssured.get(url);
 
-        //print response status code
-        System.out.println(response.statusCode());
-
-        // print response body
-        response.prettyPrint();
 
     }
 
 
 
-}
+
